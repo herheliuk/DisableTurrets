677a5b34b826f94c6a4b65bb6716ae3b49b28b7a
Andrii Herheliuk <andrii@herheliuk.com>
2026-06-16 00:46:02 +0100

    1.0,.1

--- DisableTurrets.cs
+++ DisableTurrets.cs
@@ -1,18 +1,35 @@
 namespace Oxide.Plugins;
 
-[Info("Disable Turrets", "&anhe", "1.0.0")]
+[Info("Disable Turrets", "&anhe", "1.0.1")]
 [Description("Prevents player turrets from targeting other players.")]
 public class DisableTurrets : RustPlugin
 {
-    private bool CanBeTargeted(BasePlayer player, BaseEntity entity) => (
-        !player.IsAdmin &&
-        (
-            // World
-            entity.OwnerID == 0 ||
+    private object CanBeTargeted(BaseCombatEntity target, AutoTurret entity)
+    {
+        if (entity.OwnerID == 0)
+            return null;
+        
+        return (
+            target is BasePlayer player &&
+            (
+                // Yours
+                entity.OwnerID == player.userID ||
+                // Team’s
+                player.Team.members.Contains(entity.OwnerID) == true
+            )
+        );
+    }
+
+    private object CanBeTargeted(BasePlayer player, FlameTurret entity)
+    {
+        if (entity.OwnerID == 0)
+            return null;
+
+        return (
             // Yours
             entity.OwnerID == player.userID ||
             // Team’s
             player.Team.members.Contains(entity.OwnerID) == true
-        )
-    );
+        );
+    }
 }
